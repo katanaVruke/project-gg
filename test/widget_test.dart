48e8a3da84579e98f821b5c0faafe7519ccd539c
katanaVruke <andreydi77@gmail.com>
2025-12-22 08:13:05 +0500

    x

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('My test', () {
-    expect(1 + 1, 2);
+  test('Basic CI/CD test - always passes', () {
+    expect(1 + 1, equals(2));
+    expect('CI/CD', isNotEmpty);
+  });
+
+  test('Pipeline stages test', () {
+    final stages = ['lint', 'test', 'build', 'deploy'];
+    expect(stages.length, 4);
+    expect(stages, contains('test'));
+  });
+
+  group('Mathematics', () {
+    test('addition', () => expect(2 + 2, 4));
+    test('multiplication', () => expect(3 * 3, 9));
+    test('division', () => expect(10 / 2, 5));
   });
 }
